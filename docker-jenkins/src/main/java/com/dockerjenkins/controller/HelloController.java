53e8083d2452ee7b20bfb711bd7b9c770c8bd4d2
willimqiao <1124503420@qq.com>
2019-12-05 14:01:08 +0800

    第一个项目

---
+++ docker-jenkins/src/main/java/com/dockerjenkins/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.dockerjenkins.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+    @RequestMapping("/")
+    @ResponseBody
+    public String hello() {
+        return "Hello, SpringBoot With Docker";
+    }
+}
